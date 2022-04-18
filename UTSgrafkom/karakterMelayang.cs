5e387f8e74c26bc36a7725c1c77e7566135647a9
Steven-Christando <c14200094@john.petra.ac.id>
2022-04-18 13:43:47 +0700

    commit hari ini

---
+++ UTSgrafkom/karakterMelayang.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearnOpenTK.Common;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+
+namespace UTSgrafkom
+{
+    internal class karakterMelayang
+    {
+    }
+}
